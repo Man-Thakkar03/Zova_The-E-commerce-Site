7012a43a52ed80162a70b1a6626771381d766040
Man-Thakkar03 <manthakkar2005@gmail.com>
2025-06-26 13:56:30 +0530

    Added all backend files

---
+++ E-COMMERCE/backend/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRote.js';
+
+
+
+
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudinary();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+// 🔧 Serve uploaded files statically
+app.use('/uploads', express.static('uploads'));
+
+// Routes
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart',cartRouter)
+app.use('/api/order',orderRouter)
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send("API working");
+});
+
+app.listen(port, () => console.log("🚀 Server started on port: " + port));
